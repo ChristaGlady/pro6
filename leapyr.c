b3ed044ef62a337f2b730bb42bd8c16d442ad851
ChristaGlady <christajglady7@gmail.com>
2016-06-25 08:33:39 +0530

    Create leapyr.c

---
+++ leapyr.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+  
+int main(){
+int year;
+clrscr();
+printf("Enter a year for leap year check\n");
+scanf("%d", &year);
+ 
+if(((year%4==0)&&(year%100!=0))||(year%400==0))
+{
+printf("%d is leap year\n", year);
+} 
+else {
+printf("%d is not leap year\n", year);
+}
+getch();
+return 0;
+}
